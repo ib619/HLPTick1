be1224d70482f9bc70f41d29c1d2d8e7f52fabab
igorbodnar16 <igor.bodnar.prog@gmail.com>
2022-01-17 13:47:02 +0000

    submission

--- Tick1q/Program.fs
+++ Tick1q/Program.fs
@@ -4,10 +4,93 @@
 
 // top-level subfunctions of polarToCartesianApprox (if any)
 
+// factorial function
+let fact n =
+    if n = 0
+    then 1.0
+    else List.reduce (*) [1..float n]
+
+// utility function to find index of element in a list
+let findListIndex arr (elem:float) = 
+    arr |> List.findIndex ((=)elem)
+
+//--------------helper functions for Sine Taylor Series calculation--------------//
+
+// creates a list of ints in the form [0; 1; 3; .. n]
+let taylorSine n = 
+    let list = [0..n]
+    list |> List.filter (fun x -> (x%2 = 1 || x = 0))
+
+// converts a list from taylorSine to the actual coefficients with powers and factorials
+let taylorSineCoefficient (x:float) = 
+    let calculate n = 
+        if n = 0
+        then 0.0
+        else
+            let denominator = fact n
+            (x**float n)/denominator
+    calculate
+
+//negates every even index element of a list excluding zero to produce alternating +/- pattern
+let negateEven arr =
+    let indexing (elem:float) =
+        let index = findListIndex arr elem
+        let even = (index%2 = 0)
+        if even && not (index = 0) then
+            -1.0*elem
+        else
+            elem
+    indexing
+
+
+//-------------helper functions for Cosine Taylor Series calculation-------------//
+
+// creates a list of ints in the form [0; 2; 4; .. n]
+let taylorCos n = 
+    let list = [0..n]
+    list |> List.filter (fun x -> (x%2 = 0))
+
+// converts a list from taylorCos to the actual coefficients with powers and factorials
+let taylorCosCoefficient (x:float) = 
+    let calculate n = 
+        if n = 0
+        then 1.0
+        else
+            let denominator = fact n
+            (x**float n)/denominator
+    calculate
+
+//negates every odd index element of a list to produce alternating +/- pattern
+let negateOdd arr =
+    let indexing (elem:float) =
+        let index = findListIndex arr elem
+        let odd = (index%2 = 1)
+        if odd && not (index = 0) then
+            -1.0*elem
+        else
+            elem
+    indexing
+
+//-------------------------------Sine Calculation--------------------------------//
+let sine (x:float) n = 
+    let num_list = taylorSine n
+    let coef_list = List.map (taylorSineCoefficient x) num_list
+    let coef_list_signed = List.map (negateEven coef_list) coef_list
+    List.reduce (+) coef_list_signed
+
+//------------------------------Cosine Calculation-------------------------------//
+let cosine (x:float) n = 
+    let num_list = taylorCos n
+    let coef_list = List.map (taylorCosCoefficient x) num_list
+    let coef_list_signed = List.map (negateOdd coef_list) coef_list
+    List.reduce (+) coef_list_signed
+
 /// answer to Tick1
 // the header given here is correct.
 let polarToCartesianApprox (r,theta) n = 
-    failwithf "Tick1 not yet implemented" // replace this line with your top-level implementation
+    let sine_theta = sine theta n
+    let cos_theta = cosine theta n
+    (r*cos_theta, r*sine_theta)
 
 
 //--------------------testbench code - DO NOT CHANGE-----------------------------//
